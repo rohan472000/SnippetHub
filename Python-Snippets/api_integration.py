ac43c5190a7b7fe63ad4496260feb357e8fb0418
Ahmet Anbar <ahmet.anbar@getir.com>
2023-10-01 12:15:49 +0300

    make api integration more generic

--- Python-Snippets/api_integration.py
+++ Python-Snippets/api_integration.py
@@ -10,24 +10,57 @@ Wikipedia: https://en.wikipedia.org/wiki/API
 """
 
 import requests
+from typing import Optional, Dict, Any
 
-# URL of the API endpoint
-api_url = 'https://api.github.com/users/prasad-chavan1'
 
-try:
-    # Send an HTTP GET request to the API endpoint
-    response = requests.get(api_url)
+def make_api_request(url: str, 
+                     method: str = 'GET', 
+                     params: Optional[Dict[str, Any]] = None, 
+                     data: Optional[Dict[str, Any]] = None, 
+                     headers: Optional[Dict[str, str]] = None) -> Optional[Dict[str, Any]]:
+    """
+    Send an HTTP request to the specified API endpoint.
 
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the JSON response
-        data = response.json()
+    Args:
+        url (str): The URL of the API endpoint.
+        method (str, optional): The HTTP method to use (default is 'GET').
+        params (Dict, optional): Optional query parameters for the request.
+        data (Dict, optional): Optional data to include in the request body.
+        headers (Dict, optional): Optional headers to include in the request.
 
-        # Process the data (e.g., print it)
+    Returns:
+        Optional[Dict, None]: Parsed JSON response if the request is successful, None otherwise.
+
+    Raises:
+        requests.exceptions.RequestException: If an error occurs while making the request.
+    """
+    try:
+        # Send an HTTP request to the API endpoint with optional headers, parameters, and data
+        response = requests.request(method, url, params=params, data=data, headers=headers,)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the JSON response
+            data = response.json()
+            return data
+        else:
+            # Handle non-200 status codes gracefully
+            print(f'API request failed with status code {response.status_code}. Reason: {response.text}')
+            return None
+
+    except requests.exceptions.RequestException as e:
+        # Handle any request exceptions
+        print(f'Error making the API request: {e}')
+        return None
+
+if __name__ == '__main__':
+    api_url = 'https://api.github.com/users/prasad-chavan1'  # Replace with the API URL you want to fetch
+    http_method = 'GET'  # You can change the HTTP method as needed (e.g., 'POST', 'PUT', 'DELETE')
+    
+    response_data = make_api_request(api_url, method=http_method)
+
+    if response_data is not None:
         print("Received Data:")
-        print(data)
+        print(response_data)
     else:
-        print('Failed to retrieve data from the API. Status code:', response.status_code)
-
-except requests.exceptions.RequestException as e:
-    print('Error making the API request:', e)
+        print("No data received. Check the API request for errors.")
